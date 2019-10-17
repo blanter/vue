91ce2b5a4909ac54a2f6f64f52bd7e813db04462
blanter <necromancer080@gmail.com>
2019-10-17 13:26:58 +0700

    Update vue-argon-posts.js

--- vue-argon-posts.js
+++ vue-argon-posts.js
@@ -203,4 +203,5 @@ document.getElementById('add-pemesanan').innerHTML =
 function lazyLoad(){for(var e=document.getElementsByClassName("lazy"),t=0;t<e.length;t++)isInViewport(e[t])&&(e[t].src=e[t].getAttribute("data-src"))}function isInViewport(e){var t=e.getBoundingClientRect();return t.bottom>=0&&t.right>=0&&t.top<=(window.innerHeight||document.documentElement.clientHeight)&&t.left<=(window.innerWidth||document.documentElement.clientWidth)}function registerListener(e,t){window.addEventListener?window.addEventListener(e,t):window.attachEvent("on"+e,t)}registerListener("load",lazyLoad),registerListener("scroll",lazyLoad);
 for(i=0; i<1; i++){
 lazyLoad();
-}
+};
+$("img.lazy").toggleClass("show");
