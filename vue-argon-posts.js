7a6aea05d575f9877d18b7bc4702bafd4f4e866f
blanter <necromancer080@gmail.com>
2019-10-17 13:15:50 +0700

    Update vue-argon-posts.js

--- vue-argon-posts.js
+++ vue-argon-posts.js
@@ -198,3 +198,9 @@ document.getElementById('add-pemesanan').innerHTML =
 '</select><a type="submit" class="captchaid" href="javascript:void"><span class="box-captcha"></span>Setuju Syarat dan Ketentuan</a></div>'+
 '<div class="theme-grid"><i class="icon ion-ios-globe"></i><input class="validate" id="linkform" placeholder="URL Blog *www.example.com" name="url" required="" type="url" value=""/><span class="badge text-info">Jika lebih dari 1, Pisahkan dengan spasi.</span></div>'+
 "</form>";
+
+/* Lazy Load */
+function lazyLoad(){for(var e=document.getElementsByClassName("lazy"),t=0;t<e.length;t++)isInViewport(e[t])&&(e[t].src=e[t].getAttribute("data-src"))}function isInViewport(e){var t=e.getBoundingClientRect();return t.bottom>=0&&t.right>=0&&t.top<=(window.innerHeight||document.documentElement.clientHeight)&&t.left<=(window.innerWidth||document.documentElement.clientWidth)}function registerListener(e,t){window.addEventListener?window.addEventListener(e,t):window.attachEvent("on"+e,t)}registerListener("load",lazyLoad),registerListener("scroll",lazyLoad);
+for(i=0; i<1; i++){
+lazyLoad();
+}
