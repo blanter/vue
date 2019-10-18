653e14028d9e474f10d23ff78f8e512b437a6813
blanter <necromancer080@gmail.com>
2019-10-18 08:27:55 +0700

    Update vue-argon-posts.js

--- vue-argon-posts.js
+++ vue-argon-posts.js
@@ -41,7 +41,7 @@ $(document)
 /* Whatsapp Chat Widget by www.idblanter.com */
 $(document).on("click","#send-it",function(){var a=document.getElementById("chat-input");if(""!=a.value){var b=$("#get-number").text(),c=document.getElementById("chat-input").value,d="https://web.whatsapp.com/send",e=b,f="&text="+c;if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))var d="whatsapp://send";var g=d+"?phone="+e+f;window.open(g, '_blank')}}),$(document).on("click",".informasi",function(){document.getElementById("get-number").innerHTML=$(this).children(".my-number").text(),$(".start-chat,.get-new").addClass("show").removeClass("hide"),$(".home-chat,.head-home").addClass("hide").removeClass("show"),document.getElementById("get-nama").innerHTML=$(this).children(".info-chat").children(".chat-nama").text(),document.getElementById("get-label").innerHTML=$(this).children(".info-chat").children(".chat-label").text()}),$(document).on("click",".close-chat",function(){$("#whatsapp-chat").addClass("hide").removeClass("show")}),$(document).on("click",".blantershow-chat",function(){$("#whatsapp-chat").addClass("show").removeClass("hide")});
 
-if($('.post').hasClass('hentry')) {
+if($('.product-content').hasClass('produk')) {
 /* HTML Pembayaran */
 document.getElementById('add-pembayaran').innerHTML = 
 "<div class='pay-box'><center>"+
@@ -85,6 +85,11 @@ document.getElementById('add-pemesanan').innerHTML =
 '<div class="theme-grid"><i class="icon ion-ios-globe"></i><input class="validate" id="linkform" placeholder="URL Blog *www.example.com" name="url" required="" type="url" value=""/><span class="badge text-info">Jika lebih dari 1, Pisahkan dengan spasi.</span></div>'+
 "</form>";
 };
+
+/* Halaman */
+    if($('.pageBody').hasClass('gallery')) {
+        $('.post-container').toggleClass("halaman");
+    } 
 
 /* Lazy Load */
 function lazyLoad(){for(var e=document.getElementsByClassName("lazy"),t=0;t<e.length;t++)isInViewport(e[t])&&(e[t].src=e[t].getAttribute("data-src"))}function isInViewport(e){var t=e.getBoundingClientRect();return t.bottom>=0&&t.right>=0&&t.top<=(window.innerHeight||document.documentElement.clientHeight)&&t.left<=(window.innerWidth||document.documentElement.clientWidth)}function registerListener(e,t){window.addEventListener?window.addEventListener(e,t):window.attachEvent("on"+e,t)}registerListener("load",lazyLoad),registerListener("scroll",lazyLoad);
@@ -208,8 +213,3 @@ document.getElementById('preview').href = preview;
 var changelog = document.getElementById('go-changelog').innerHTML;
 document.getElementById('add-changelog').innerHTML = changelog;
 document.getElementById('go-changelog').innerHTML = '';
-
-/* Halaman */
-    if($('.pageBody').hasClass('gallery')) {
-        $('.post-container').toggleClass("halaman");
-    } 
